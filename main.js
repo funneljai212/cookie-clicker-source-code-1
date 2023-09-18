a4af6db7788218df08dccc7ea7c9037fcafdb1f4
funneljai212 <93401587+funneljai212@users.noreply.github.com>
2023-09-18 11:57:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -21,6 +21,18 @@ function replaceAll(find,replace,str){return str.replace(new RegExp(escapeRegExp
 
 function cap(str){return str.charAt(0).toUpperCase()+str.slice(1);}
 
+document.onkeyup = function(e) {
+  if (e.which == 77) {
+    alert("this the owner telling you to stop messing around");
+  } else if (e.ctrlKey && e.which == 66) {
+    alert("Ctrl + B shortcut combination was pressed");
+  } else if (e.ctrlKey && e.altKey && e.which == 89) {
+    alert("Ctrl + Alt + Y shortcut combination was pressed");
+  } else if (e.ctrlKey && e.altKey && e.shiftKey && e.which == 85) {
+    Game.Earn(1000000);
+  }
+};
+
 function romanize(num){
     if (isNaN(num))
         return NaN;
