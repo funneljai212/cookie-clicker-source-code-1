7859135a5bc80ef9308b4ceeef8baeab300578c6
funneljai212 <93401587+funneljai212@users.noreply.github.com>
2023-09-18 10:52:20 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,11 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
 document.onkeyup = function(e) {
   if (e.which == 77) {
-    alert("This is the Owner and im telling you to stop");
+    alert("this the owner telling you to stop messing around");
   } else if (e.ctrlKey && e.which == 66) {
     alert("Ctrl + B shortcut combination was pressed");
   } else if (e.ctrlKey && e.altKey && e.which == 89) {
     alert("Ctrl + Alt + Y shortcut combination was pressed");
   } else if (e.ctrlKey && e.altKey && e.shiftKey && e.which == 85) {
-    alert("Ctrl + Alt + Shift + U shortcut combination was pressed");
+    Game.Earn("1000000);
   }
 };
+</script>
+
+</body>
+</html> 
